344a8f6a8849da836688acce721663b113e3c95e
babysakura <141540491+babysakura@users.noreply.github.com>
2024-01-14 11:46:53 +0900

    chenge https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (\APP::environment(['production'])){
-            \URL::forseScheme('https');
+        if  (App::environment(['production'])){
+            URL::forceScheme('https');
         }
     }
 }
